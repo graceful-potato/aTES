318297680e6a4f2bd66c8ff4c6af781aa7a2b450
Graceful Potato <gracefulpotatow@gmail.com>
2023-08-18 16:07:59 +0400

    Fix after review.

--- task-tracker/app/controllers/api/v1/tasks_controller.rb
+++ task-tracker/app/controllers/api/v1/tasks_controller.rb
@@ -26,20 +26,31 @@ class Api::V1::TasksController < ApplicationController
     if @task.save
       @task.reload # load public_id from database
 
+      data = {
+        public_id: @task.public_id,
+        description: @task.description,
+        completed_at: @task.completed_at,
+        assignee_id: @task.assignee_id,
+        fee: @task.fee,
+        reward: @task.reward,
+        created_at: @task.created_at
+      }
+
+      # Stream event
       event = {
         event_name: "TaskCreated",
-        data: {
-          public_id: @task.public_id,
-          description: @task.description,
-          completed_at: @task.completed_at,
-          assignee_id: @task.assignee_id,
-          fee: @task.fee,
-          reward: @task.reward,
-          created_at: @task.created_at
-        }
+        data: data
       }
 
       Karafka.producer.produce_sync(topic: "tasks-stream", payload: event.to_json)
+
+      # Business event
+      event = {
+        event_name: "TaskAdded",
+        data: data
+      }
+
+      Karafka.producer.produce_sync(topic: "tasks-lifecycle", payload: event.to_json)
 
       render json: @task, status: :created
     else
@@ -64,7 +75,6 @@ class Api::V1::TasksController < ApplicationController
 
       Karafka.producer.produce_sync(topic: "tasks-stream", payload: event.to_json)
 
-
       render json: @task
     else
       render json: @task.errors, status: :unprocessable_entity
@@ -72,22 +82,22 @@ class Api::V1::TasksController < ApplicationController
   end
 
   # DELETE /tasks/1
-  def destroy
-    if current_account.role != "admin"
-      return render json: { error: "Forbidden" }, status: :forbidden
-    end
+  # def destroy
+  #   if current_account.role != "admin"
+  #     return render json: { error: "Forbidden" }, status: :forbidden
+  #   end
 
-    event = {
-      event_name: "TaskDeleted",
-      data: {
-        public_id: @task.public_id
-      }
-    }
+  #   event = {
+  #     event_name: "TaskDeleted",
+  #     data: {
+  #       public_id: @task.public_id
+  #     }
+  #   }
 
-    Karafka.producer.produce_sync(topic: "tasks-stream", payload: event.to_json)
+  #   Karafka.producer.produce_sync(topic: "tasks-stream", payload: event.to_json)
 
-    @task.destroy
-  end
+  #   @task.destroy
+  # end
 
   def complete
     task = current_account.tasks.find(params[:id])
@@ -101,7 +111,7 @@ class Api::V1::TasksController < ApplicationController
         }
       }
   
-      Karafka.producer.produce_sync(topic: "tasks", payload: event.to_json)
+      Karafka.producer.produce_sync(topic: "tasks-lifecycle", payload: event.to_json)
 
       render json: task
     else
@@ -123,7 +133,7 @@ class Api::V1::TasksController < ApplicationController
         data: tasks_in_progress.map { |task| { public_id: task.public_id, assignee_id: task.assignee_id } }
       }
   
-      Karafka.producer.produce_sync(topic: "tasks", payload: event.to_json)
+      Karafka.producer.produce_sync(topic: "tasks-lifecycle", payload: event.to_json)
 
     end
 
