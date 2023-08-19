23a8726f13078cdaf50819233a3f6791878c728f
Graceful Potato <gracefulpotatow@gmail.com>
2023-08-19 04:33:52 +0400

    Fix application loading sequence.

--- analytics/rails-entrypoint.sh
+++ analytics/rails-entrypoint.sh
@@ -5,21 +5,22 @@ if [ -f tmp/pids/server.pid ]; then
   rm -f tmp/pids/server.pid;
 fi
 
+# Wait for postgres
 until PGPASSWORD=$DATABASE_PASSWORD psql -h "$DATABASE_HOST" -U "$DATABASE_USER" -d "postgres" -c '\q'; do
-  echo "Postgres is unavailable - sleeping"
   sleep 1
 done
 
+# Wait for kafka
 until nc -z kafka 9092; do
-    echo "Kafka is unavailable - sleeping"
     sleep 1
 done
 
+# Create and initilize db when running container first time.
 DATABASE_EXISTS=$(PGPASSWORD=$DATABASE_PASSWORD psql -h "$DATABASE_HOST" -U "$DATABASE_USER" -tAc "SELECT 1 FROM pg_database WHERE datname='${DATABASE_NAME}_${RAILS_ENV}'")
-if [ "$DATABASE_EXISTS" = "1" ]; then
-  echo "Database exists"
-else
-  rails db:setup
+if [ "$DATABASE_EXISTS" != "1" ]; then
+  bundle exec rails db:create
+  bundle exec rails db:migrate
+  bundle exec rails db:seed
 fi
 
 exec "$@"
---
+++ task-tracker/kafka_server-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+# Wait for postgres
+until PGPASSWORD=$DATABASE_PASSWORD psql -h "$DATABASE_HOST" -U "$DATABASE_USER" -d "postgres" -c '\q'; do
+  sleep 1
+done
+
+# Wait for kafka
+until nc -z kafka 9092; do
+    sleep 1
+done
+
+# Wait for rails app
+# TODO: extract port into env variable
+until nc -z task-tracker 3001; do
+  sleep 1
+done
+
+exec "$@"
